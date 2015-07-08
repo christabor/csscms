49c759e3b2c1e8fffc036f16b54432d011afe230
christabor <dxdstudio@gmail.com>
2015-07-07 21:17:38 -0700

    Make demo parser better.

--- demo/demo.py
+++ demo/demo.py
@@ -1,20 +1,22 @@
 from csscms.parser import InputBuilder
 import os
 
-# try:
-print('[DEBUG] Running demo')
-name = raw_input('Which one (choose a number)?'
-                 '\n1. Bootstrap3 \n2. Font-awesome \n3. Test\n=> ')
+try:
+    print('[DEBUG] Running demo')
+    output = [
+        'bootstrap3',
+        'fa',
+        'test-inputs',
+        'simple'
+    ]
+    name = raw_input('Which one (choose a number)?\n{}\n=> '.format('\n'.join(
+        ['{}: {} '.format(k + 1, v) for k, v in enumerate(output)])))
 
+    filename = output[int(name) - 1]
 
-output = {
-    '1': 'bootstrap3',
-    '2': 'fa',
-    '3': 'test-inputs',
-    '4': 'simple'
-}
-InputBuilder('{}/{}.css'.format(
-    os.getcwd(), output[name])).generate().save(
-        '{}-output.html'.format(output[name]))
-# except IOError:
-#     print('[ERROR] Could not load file or generate inputs')
+    InputBuilder('{}/{}.css'.format(
+        os.getcwd(), filename)).generate().save(
+            '{}-output.html'.format(filename))
+
+except IOError:
+    print('[ERROR] Could not load file or generate inputs')
